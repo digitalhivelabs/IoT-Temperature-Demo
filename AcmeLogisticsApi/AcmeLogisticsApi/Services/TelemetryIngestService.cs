5d0b26fadc3761ec69c9e9e9af12c7b1e1e89347
Majahide Payan <majahide.payan@hotmail.com>
2026-07-10 20:57:52 -0700

    Added the alert lgic NOTE: Needs refactor the code is dirty

--- AcmeLogisticsApi/AcmeLogisticsApi/Services/TelemetryIngestService.cs
+++ AcmeLogisticsApi/AcmeLogisticsApi/Services/TelemetryIngestService.cs
@@ -11,6 +11,10 @@ namespace AcmeLogisticsApi.Services
         private readonly EventProcessorClient _processor;
         private readonly BlobContainerClient _telemetryContainer;
 
+        //Alerts (needs to be refactores and no hard coded values.)
+        private int consecutiveHighReadings = 0;
+        private const double TemperatureThreshold = 8.0;
+
         public TelemetryIngestService(string eventHubConnectionString, string eventHubName, string blobConnectionString)
         {
             var blobClient = new BlobContainerClient(blobConnectionString, "checkpoints");
@@ -27,12 +31,48 @@ namespace AcmeLogisticsApi.Services
                 string body = args.Data.EventBody.ToString();
                 var telemetry = JsonSerializer.Deserialize<TelemetryMessage>(body);
 
+
+                string blobName = string.Empty;
+                BlobClient blobClient;
+
+                #region Alert (needs to be refactored)
+                if (telemetry?.TemperatureC > TemperatureThreshold)
+                {
+                    consecutiveHighReadings++;
+                }
+                else
+                {
+                    consecutiveHighReadings = 0;
+                }
+
+                if (consecutiveHighReadings >= 3)
+                {
+                    var alert = new
+                    {
+                        deviceId = telemetry?.DeviceId,
+                        temperature = telemetry?.TemperatureC,
+                        timestamp = telemetry?.Timestamp,
+                        message = "Alert sustained: temperature exceeds"
+                    };
+
+                    // Guardar alerta en Blob Storage o DB
+                    blobName = $"alert-{DateTime.UtcNow:yyyyMMddHHmmssfff}.json";
+                    blobClient = _telemetryContainer.GetBlobClient(blobName);
+                    using var streamAlert = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(alert)));
+                    await blobClient.UploadAsync(streamAlert, overwrite: true);
+
+                    // Reinicia contador para evitar múltiples alertas seguidas
+                    consecutiveHighReadings = 0;
+                }
+                #endregion Alert
+
+
                 // Saves to a txt File (testing before adding Blob storage)
                 //await File.AppendAllTextAsync("telemetry.log", body + Environment.NewLine);
-                
+
                 // Saving the data to Blob Storage Telemtry Container
-                string blobName = $"telemetry-{DateTime.UtcNow:yyyyMMddHHmmssfff}.json";
-                BlobClient blobClient = _telemetryContainer.GetBlobClient(blobName);
+                blobName = $"telemetry-{DateTime.UtcNow:yyyyMMddHHmmssfff}.json";
+                blobClient = _telemetryContainer.GetBlobClient(blobName);
 
                 using var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(body));
                 await blobClient.UploadAsync(stream, overwrite: true);
