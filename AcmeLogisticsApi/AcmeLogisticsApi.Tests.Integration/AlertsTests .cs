3694f1661e56b1b4058698192edfb066829d0968
Majahide Payan <majahide.payan@hotmail.com>
2026-07-11 10:02:21 -0700

    Added some unit test, demo porpouses

---
+++ AcmeLogisticsApi/AcmeLogisticsApi.Tests.Integration/AlertsTests .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using FluentAssertions;
+
+namespace AcmeLogisticsApi.Tests.Integration
+{
+    public class AlertsTests : IntegrationTestBase
+    {
+        public AlertsTests(WebApplicationFactory<Program> factory) : base(factory) { }
+
+        [Fact]
+        public async Task OperatorCanAcknowledgeAlert()
+        {
+            // Emulate that already exist.
+            var response = await _client.PutAsJsonAsync("/api/telemetry/alerts/alert-12345/acknowledge", new { });
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+
+            var alert = await response.Content.ReadFromJsonAsync<dynamic>();
+            ((bool)alert!.Acknowledged).Should().BeTrue();
+        }
+    }
+}
---
+++ AcmeLogisticsApi/AcmeLogisticsApi.Tests.Integration/IntegrationTestBase .cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AcmeLogisticsApi.Tests.Integration
+{
+    public class IntegrationTestBase: IClassFixture<WebApplicationFactory<Program>>
+    {
+        protected readonly HttpClient _client;
+
+        public IntegrationTestBase(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+    }
+}
